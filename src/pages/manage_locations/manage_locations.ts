0ec86400fd17e2ecdd5d5709dd893ffb19675f57
Bernard Brink <bernardb@miway.co.za>
2018-02-02 15:37:04 +0200

    Removed unused properties and imports

--- src/pages/manage_locations/manage_locations.ts
+++ src/pages/manage_locations/manage_locations.ts
@@ -1,12 +1,10 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
 
 @Component({
     selector: 'manage-locations',
     templateUrl: 'manage_locations.html'
 })
 export class ManageLocationsPage {
-    constructor(navCtrl: NavController) {
-
+    constructor() {
     }
 }
--- src/pages/reports/reports.ts
+++ src/pages/reports/reports.ts
@@ -1,12 +1,10 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
 
 @Component({
     selector: 'reports',
     templateUrl: 'reports.html'
 })
 export class ReportsPage {
-    constructor(navCtrl: NavController) {
-
+    constructor() {
     }
 }
